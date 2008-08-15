8770265650aacb9496d4ea219150436b857f9ec8
jerome.renard <devnull@localhost>
2008-08-15 15:36:33 +0000

    - delete doc directory - moved INSTALL to root directory - moved source files to src - fixed issue 2 - deletes original Makefile - added empty 'config' dir

--- modules.mk
+++
@@ -1,4 +0,0 @@
-mod_memcached_include.la: mod_memcached_include.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_memcached_include.lo
-DISTCLEAN_TARGETS = modules.mk
-shared =  mod_memcached_include.la
